a9f46e45642e2388ca07a8e4f6da81e2f0507a35
manhcuong1122 <cuongdinhmanh1122@gmail.com>
2023-06-18 18:07:44 +0700

    Add messages components

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYNzaPelHgrEejyS5LIOs4q6YRmPNZr8O4zA&usqp=CAU"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYNzaPelHgrEejyS5LIOs4q6YRmPNZr8O4zA&usqp=CAU"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
