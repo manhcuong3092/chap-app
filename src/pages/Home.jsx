64cfc8c80b8cb74c85e26f2ee4c6b8500498c0cb
manhcuong1122 <cuongdinhmanh1122@gmail.com>
2023-06-18 16:07:12 +0700

    add components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,16 @@
-function Home() {
-  return <div>Home!</div>;
-}
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import Chat from "../components/Chat";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="container">
+        <Sidebar />
+        <Chat />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
